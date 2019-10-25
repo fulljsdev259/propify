7f6c179c6efd3b4bf54264b3579b55d2f8416abe
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-10-25 22:04:13 +0400

    remove position fileld in PM

--- app/Transformers/PropertyManagerTransformer.php
+++ app/Transformers/PropertyManagerTransformer.php
@@ -29,7 +29,6 @@ class PropertyManagerTransformer extends BaseTransformer
             'first_name' => $model->first_name,
             'last_name' => $model->last_name,
             'profession' => $model->profession,
-            'position' => $model->position,
             'slogan' => $model->slogan,
             'xing_url' => $model->xing_url,
             'linkedin_url' => $model->linkedin_url,
