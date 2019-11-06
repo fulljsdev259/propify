e3e949031f3a7bbdc0d41c80cd6a77f15b53e072
TigerDevloper <tigerdeveloper@outlook.com>
2019-11-06 02:13:43 -0500

    update mass edit style

--- resources/js/views/Admin/Requests/index.vue
+++ resources/js/views/Admin/Requests/index.vue
@@ -512,7 +512,7 @@
                 this.closeModal();
                 this.fetchMore();
                 if(resp.data.success)
-                    displaySuccess(resp.data.message);
+                    displaySuccess(resp.data);
 
             },
             async massAssignManagers() {
@@ -528,7 +528,7 @@
                 this.closeModal();
                 this.fetchMore();
                 if(resp.data.success)
-                    displaySuccess(resp.data.message);
+                    displaySuccess(resp.data);
 
             },
             async massChangeStatus() {
@@ -544,7 +544,7 @@
                 this.closeModal();
                 this.fetchMore();
                 if(resp.data.success)
-                    displaySuccess(resp.data.message);
+                    displaySuccess(resp.data);
                 
             },
             async remoteSearchManagers(search) {
@@ -696,6 +696,7 @@
         }
 
         .switch-wrapper {
+            margin-top: 10px;
             margin-bottom: 0;
 
             .switcher__desc {
