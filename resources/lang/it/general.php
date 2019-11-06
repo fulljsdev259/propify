39f7f7b6894cfd2fa27588a49c98fa05033dbd18
Sandip <sb@gocaution.ch>
2019-11-06 18:05:21 +0530

    deleted and new_resident_pinboard_created audit entry

--- resources/lang/it/general.php
+++ resources/lang/it/general.php
@@ -347,6 +347,7 @@ return [
                     'general' => [
                         'created' => 'Creato',
                         'updated' => 'Aggiornamenti',
+                        'deleted' => 'Cancellata',
                         'provider_assigned' => 'Fornitore assegnato',
                         'provider_unassigned' => 'Fornitore disimpegnato',
                         'provider_notified' => 'Fornitore notificato',
@@ -357,13 +358,16 @@ return [
                         'building_assigned' => 'Costruire assegnato',
                         'building_unassigned' => 'Costruire disimpegnato',
                         'media_uploaded' => 'Media caricati',
-                        'media_deleted' => 'Supporti cancellati'
+                        'media_deleted' => 'Supporti cancellati',
+                        'notifications_sent' => 'Notifica inviata',
+                        'new_resident_pinboard_created' => 'Nuova bacheca vicina',
                     ]                    
                 ],
                 'content' => [                    
                     'general' => [
                         'created' => ':userName ha creato questo :auditable_type',
                         'updated' => 'La :fieldname è cambiata da :old al :new.',
+                        'deleted' => ':userName ha cancellato questo :auditable_type',
                         'provider_assigned' => ':providerName è stato assegnato come fornitore.',
                         'provider_unassigned' => 'Fornitore di servizi :providerName non è stato assegnato.',
                         'manager_assigned' => ':propertyManagerFirstName :propertyManagerLastName è stato assegnato come manager.',
@@ -376,6 +380,8 @@ return [
                         'quarter_unassigned' => ':quarterName Quartiere non assegnato',
                         'building_assigned' => ':buildingName Costruire assegnato',
                         'building_unassigned' => ':buildingNam Costruire senza assegnazione',
+                        'notifications_sent' => 'È stata inviata una notifica per questo tipo :auditable_type',
+                        'new_resident_pinboard_created' => "Aggiunto un nuovo vicino di casa per l'utente :userName",
                     ],                                   
                 ],
             ],
