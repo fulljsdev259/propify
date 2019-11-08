7a414ef7032714756e8b312511a98d7c5b013165
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-11-09 00:16:10 +0400

    improvement media upload

--- app/Traits/SaveMediaUploads.php
+++ app/Traits/SaveMediaUploads.php
@@ -2,53 +2,87 @@
 
 namespace App\Traits;
 
+use App\Models\Media;
+use Illuminate\Support\Arr;
+
 trait SaveMediaUploads
 {
-    public function saveMediaUploads(\App\Models\Model $model, $data, $disableAuditing = false)
+    public function saveMediaUploads(\App\Models\Model $model, $data)
     {
+        $audit = $this->getAudit($model);
         $existingMedia = $model->media;
+
         if (empty($data['media'])) {
-            $existingMedia->each(function ($media) {
-                $media->delete();
-            });
+            $this->deleteOldImages($existingMedia, $audit);
+            $model->addMediaInAudit($audit, $existingMedia, collect(), $existingMedia);
+
             return $model;
         }
 
-        $media = $data['media'];
-        if (is_string($media)) {
-            $media = [
-                $media
-            ];
-        }
-
+        $media = Arr::wrap($data['media']);
         $deletedMedia = $existingMedia->whereNotIn('id', collect($media)->pluck('id'));
-        $deletedMedia->each(function ($media) {
-            $media->delete();
-        });
-
-        if ($model->wasRecentlyCreated) {
-            $audit = null; // not need merge in audit
-            $disableAuditing = true;
-        } else {
-            $audit = $model->relationExists('audit') ? $model->audit : null;
-        }
+        $this->deleteOldImages($existingMedia, $audit);
 
         $savedMedia = [];
         foreach ($media as $mediaData) {
             if (is_string($mediaData)) {
-                $savedMedia[] = $this->uploadFile('media', $mediaData, $model, $audit, $disableAuditing);
+                $savedMedia[] = $this->uploadFile('media', $mediaData, $model, null, true);
             } elseif (!is_array($mediaData)) {
                 continue;
             }
             if (isset($mediaData['media']) && is_string($mediaData['media'])) {
-                $saved =  $this->uploadFile('media', $mediaData['media'], $model, $audit, $disableAuditing);
+                $saved =  $this->uploadFile('media', $mediaData['media'], $model, null, true);
                 if ($saved) {
                     $savedMedia[] = $saved;
                 }
             }
         }
 
+
+        $model->addMediaInAudit($audit, $existingMedia, collect(), $deletedMedia);
         $model->setRelation('media', $model->newCollection($savedMedia));
         return $model;
+    }
+
+    /**
+     * @param $model
+     * @return null
+     */
+    protected function getAudit($model)
+    {
+        if ($model->wasRecentlyCreated) {
+            return null; // not need merge in audit
+        }
+
+        if ($model->relationExists('audit')) {
+            return $model->audit;
+        }
+
+        return $model->makeNewAudit();
+    }
+
+    /**
+     * @param $oldMedias
+     * @param $model
+     * @param $audit
+     * @return bool
+     */
+    protected function deleteOldImages($oldMedias, $audit)
+    {
+        if ($oldMedias->isEmpty()) {
+            return true;
+        }
+
+        if ($audit) {
+            Media::disableAuditing();
+        }
+        $oldMedias->each(function ($media) {
+            $media->delete();
+        });
+
+        if ($audit) {
+            Media::enableAuditing();
+        }
+        return true;
     }
 }
