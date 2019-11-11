fda38933eef20658eead22238e4168e986111a8f
TigerDevloper <tigerdeveloper@outlook.com>
2019-11-11 01:58:29 -0500

    remove dead code

--- resources/js/mixins/adminQuartersMixin.js
+++ resources/js/mixins/adminQuartersMixin.js
@@ -185,15 +185,6 @@ export default (config = {}) => {
                 this.getStates();
                 this.types = Object.entries(this.$constants.quarters.type).map(([value, label]) => ({value: +value, name: this.$t(`models.quarter.types.${label}`)}))
                 this.assignment_types = Object.entries(this.$constants.quarters.assignment_type).map(([value, label]) => ({value: +value, name: this.$t(`models.quarter.assignment_types.${label}`)}))
-                let temp_assignment_constants = {
-                    1: "fortimo_employees",
-                    2: "management",
-                    3: "initial_letting",
-                    4: "caretaker",
-                    5: "administration"
-                }
-                //this.assignment_types = Object.entries(temp_assignment_constants).map(([value, label]) => ({value: +value, name: this.$t(`models.quarter.assignment_types.${label}`)}))
-
             },
         },
         watch: {
