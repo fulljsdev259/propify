d680a4056021594395814f7cdaffd1be2e533d60
TigerDevloper <tigerdeveloper@outlook.com>
2019-11-13 12:17:50 -0500

    update

--- resources/js/components/WorkflowForm.vue
+++ resources/js/components/WorkflowForm.vue
@@ -15,9 +15,9 @@
             <el-col :md="12">
                 <el-form-item 
                             :rules="validationRules.category"
-                            prop="category">
+                            prop="category_id">
                     <el-select :placeholder="$t('general.placeholders.select')" style="display: block" 
-                                v-model="model.category">
+                                v-model="model.category_id">
                         <el-option
                                 :key="category.id"
                                 :label="$t(`models.request.category_list.${category.name}`)"
@@ -156,7 +156,7 @@
                 global: '',
                 model: {
                     title: '',
-                    category: null,
+                    category_id: null,
                     assignList: '',
                     assign: [],
                     selectedWorkflowBuilding: [],
@@ -175,7 +175,7 @@
                         required: true,
                         message: this.$t('models.quarter.required')
                     }],
-                    category: [{
+                    category_id: [{
                         required: true,
                         message: this.$t('models.quarter.required')
                     }],
@@ -202,7 +202,7 @@
                             this.model.selectedWorkflowBuildingData.push(item)
                         })
                         
-                        this.model.categoryData = this.categories.find(item => item.id == this.model.category)
+                        let category = this.categories.find(item => item.id == this.model.category_id)
 
                         let to_users = []
 
@@ -220,8 +220,8 @@
 
                         let payload = {
                             title: this.model.title,
-                            category_id: this.model.category,
-                            category: this.model.categoryData,
+                            category_id: this.model.category_id,
+                            category: category,
                             building_ids: this.model.selectedWorkflowBuilding,
                             buildings: this.model.selectedWorkflowBuildingData,
                             to_user_ids: this.model.selectedWorkflowToUser,
@@ -357,7 +357,7 @@
 
             if(this.mode == 'edit') {
                 this.model.title = this.data.title
-                this.model.category = this.data.category_id
+                this.model.category_id = this.data.category_id
                 this.model.selectedWorkflowBuilding = this.data.building_ids
                 this.model.selectedWorkflowToUser = this.data.to_user_ids
                 this.model.selectedWorkflowCcUser = this.data.cc_user_ids
