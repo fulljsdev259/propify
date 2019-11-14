77094ed6a3519c3c910ecf34d90b070f548c843a
TigerDevloper <tigerdeveloper@outlook.com>
2019-11-14 11:15:13 -0500

    remove payer

--- resources/js/mixins/adminRequestsMixin.js
+++ resources/js/mixins/adminRequestsMixin.js
@@ -42,7 +42,6 @@ export default (config = {}) => {
                     component: '',
                     keyword: '',
                     keywords: [],
-                    payer: '',
                     property_managers: [],
                     media: [],
                     sub_category_id: null,
@@ -108,7 +107,6 @@ export default (config = {}) => {
                 locations: [],
                 rooms: [],
                 capture_phases: [],
-                payers: [],
                 showSubCategory: false,
                 showPayer: false,
                 showLocation: false,
@@ -420,7 +418,7 @@ export default (config = {}) => {
                 this.rooms = Object.entries(this.$constants.requests.room).map(([value, label]) => ({value: +value, name: this.$t(`models.request.room.${label}`)}))
                 this.capture_phases = Object.entries(this.$constants.requests.capture_phase).map(([value, label]) => ({value: +value, name: this.$t(`models.request.capture_phase.${label}`)}))
                 this.qualifications = Object.entries(this.$constants.requests.qualification).map(([value, label]) => ({value: +value, name: this.$t(`models.request.qualification.${label}`)}))
-                this.payers = Object.entries(this.$constants.requests.payer).map(([value, label]) => ({value: +value, name: this.$t(`models.request.payer.${label}`)}))
+                //this.payers = Object.entries(this.$constants.requests.payer).map(([value, label]) => ({value: +value, name: this.$t(`models.request.payer.${label}`)}))
                 this.categories = this.$constants.requests.categories_data.tree
 
                 if(this.model.category_id)
