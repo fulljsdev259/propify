6954c73eb61efd73cf035e2009312cf6ea312bf2
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-11-14 15:57:03 +0400

    remove payer col in request table

--- app/Http/Controllers/API/UtilsAPIController.php
+++ app/Http/Controllers/API/UtilsAPIController.php
@@ -308,7 +308,7 @@ class UtilsAPIController extends AppBaseController
             'location' => Request::Location,
             'room' => Request::Room,
             'capture_phase' => Request::CapturePhase,
-            'payer' => Request::Payer,
+            'payer' => Request::Payer, // @TODO delete
             'categories_data' =>  [
                 'categories' => Request::Category,
                 'sub_categories' => Request::SubCategory,
