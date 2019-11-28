96af0cc0b86c82f708fe617c6e5b28192a2438f4
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-11-28 20:02:03 +0400

    improvemrnt

---
+++ app/Criteria/Quarter/IncludeForOrderCriteria.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Criteria\Quarter;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Prettus\Repository\Contracts\CriteriaInterface;
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Class FilterByUserRoleCriteria
+ * @package App\Criteria\Quarter
+ */
+class IncludeForOrderCriteria implements CriteriaInterface
+{
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Apply criteria in query repository
+     *
+     * @param Builder|Model $model
+     * @param RepositoryInterface $repository
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    public function apply($model, RepositoryInterface $repository)
+    {
+        $model->when($this->request->orderBy == 'requests_count', function ($q) {
+                $q->withCount('requests');
+            })
+            ->when($this->request->orderBy == 'buildings_count', function ($q) {
+                $q->withCount('buildings');
+            });
+
+        return $model;
+    }
+}
