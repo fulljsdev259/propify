0f11907a5221090433e5d51850e1622406410dc7
TigerDevloper <tigerdeveloper@outlook.com>
2019-12-02 10:11:56 -0500

    update building name

--- resources/js/components/RelationListing.vue
+++ resources/js/components/RelationListing.vue
@@ -215,8 +215,7 @@
                     <div v-else-if="column.type === 'companyName'" class="normal">
                         {{scope.row.company_name}}
                     </div>
-                    <div v-else-if="column.type === 'buildingName'" class="normal">
-                        asdfasdfasdf             
+                    <div v-else-if="column.type === 'buildingName'" class="normal">           
                         {{scope.row.address ? scope.row.address.street + ' ' + scope.row.address.house_num : ''}}                
                     </div>
                     <div v-else-if="column.type === 'residentName'" class="normal"> 
