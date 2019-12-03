4ed13364149be7d41ec232450d683dd7b84fa59e
ruslankharakhashyan <ruslankharakhashyan@gmail.com>
2019-12-03 08:01:13 +0300

    Top actions button space issue fix

--- resources/js/views/Admin/Quarters/index.vue
+++ resources/js/views/Admin/Quarters/index.vue
@@ -2,7 +2,8 @@
     <div class="quarters list-view">
         <heading :title="$t('models.quarter.title')" icon="icon-share" shadow="heavy" :searchBar="true" @search-change="search=$event">
             <template>
-                <list-check-box />
+                <list-check-box 
+                    @clicked="localStorage.setItem('')"/>
             </template>
             <template v-if="$can($permissions.create.quarter)">
                 <el-button 
@@ -20,7 +21,7 @@
            
             <template>
                 <el-dropdown placement="bottom" trigger="click" @command="handleMenuClick">
-                    <el-button size="mini" class="el-button--transparent">
+                    <el-button size="mini" class="el-button--transparent more-actions">
                         <i class="el-icon-more" style="transform: rotate(90deg)"></i>
                     </el-button>
                     <el-dropdown-menu slot="dropdown">
@@ -222,7 +223,7 @@
                         data: []
                     },{
                         name: this.$t('general.filters.my_filters'),
-                        type: 'select',
+                        type: 'popover',
                         key: 'my_filter',
                         data: []
                     }
@@ -364,6 +365,9 @@
 
                 return buildings.data
             },
+            selectionChanged(items) {
+                this.selectedItems = items;
+            }
         },
         async created() {
             this.getRoles();
