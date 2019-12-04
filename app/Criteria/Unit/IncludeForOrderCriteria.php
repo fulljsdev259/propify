58fe84177ca0c2bb16c8619bfc2fc6daf0bde53f
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-12-04 12:28:30 +0400

    orders settings

---
+++ app/Criteria/Unit/IncludeForOrderCriteria.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Criteria\Unit;
+
+use App\Models\Relation;
+use App\Models\Unit;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Prettus\Repository\Contracts\CriteriaInterface;
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Class FilterByUserRoleCriteria
+ * @package App\Criteria\Quarter
+ */
+class IncludeForOrderCriteria implements CriteriaInterface
+{
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Apply criteria in query repository
+     *
+     * @param Builder|Model $model
+     * @param RepositoryInterface $repository
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    public function apply($model, RepositoryInterface $repository)
+    {
+        $sortedBy = strtolower($this->request->sortedBy) == 'asc' ? 'asc' : 'desc';
+        $model->when($this->request->orderByRaw == 'status', function ($q) use ($sortedBy) {
+           $q->orderByRaw('(SELECT status FROM relations 
+                WHERE units.id = relations.unit_id
+                order by id desc limit 1) ' . $sortedBy
+            );
+        })->when($this->request->orderBy == 'requests_count', function ($q) use ($sortedBy) {
+           $q->withCount('requests');
+        });
+
+
+        return $model;
+    }
+}
