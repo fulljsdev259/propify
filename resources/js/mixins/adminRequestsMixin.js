130b1ff74a8ccafcaece0f4fe596b9520e0b553c
ruslankharakhashyan <ruslankharakhashyan@gmail.com>
2019-12-06 15:49:18 +0300

    Request Edit address issue fix

--- resources/js/mixins/adminRequestsMixin.js
+++ resources/js/mixins/adminRequestsMixin.js
@@ -629,7 +629,7 @@ export default (config = {}) => {
 
                         //@TODO : check relation.building after api is fixed
                         //this.$set(this.model, 'building', data.relation.building.address.street + ' ' + data.relation.building.address.house_num);
-                        this.address = data.relation.address
+                        // this.address = data.relation.address
                         //this.relations = resp.data.resident.relations.filter(item => item.status == 1)
                         this.model.relation_id = data.relation.id
                         await this.getConversations();
