1fef49c6bd083933a887cbd1c2c248f033ef5e2a
greatheat57 <greatheat57@gmail.com>
2019-12-09 21:06:04 +0100

    first task fix

--- resources/js/views/Auth/3/Login.vue
+++ resources/js/views/Auth/3/Login.vue
@@ -32,7 +32,7 @@
                     <el-checkbox>{{$t('general.stay_logged_in')}}</el-checkbox>
                 </el-col>
                 <el-col :xl="12" :lg="12" :md="24" :sm="24" :xs="24" class="login-sidebar pwd_reset">
-                    <router-link :to="{name: 'forgot'}">
+                    <router-link :to="{name: 'newForgot'}">
                         <el-button type="text">
                             {{$t('general.forgot_password')}}
                         </el-button>
@@ -42,11 +42,13 @@
             </el-form-item>
             <el-form-item>
                 <el-row>
-                    <el-col :xl="7" :lg="12" :md="12" :sm="24" :xs="24" class="login-sidebar login_act">
+                    <el-col :xl="8" :lg="12" :md="12" :sm="24" :xs="24" class="login-sidebar login_act">
                         <el-button type="info" plain class="text-center w90p" @click="submit" ref="prev">{{$t('general.login')}}</el-button>
                     </el-col>
-                    <el-col :xl="7" :lg="12" :md="12" :sm="24" :xs="24" class="login-sidebar login_act register_act">
-                        <el-button type="info" plain class="text-center w90p" ref="prev">{{$t('general.register')}}</el-button>
+                    <el-col :xl="8" :lg="12" :md="12" :sm="24" :xs="24" class="login-sidebar login_act register_act">
+                        <router-link :to="{name: 'nwewActivateAccount'}" class="el-menu-item-link">
+                            <el-button type="info" plain class="text-center w90p" ref="prev">{{$t('general.register')}}</el-button>
+                        </router-link>
                     </el-col>
                 </el-row>
                 <div class="login_footer">
@@ -188,6 +190,7 @@
    
 </style>
 <style lang="scss">
+    .login2-container{font-family: 'RadikalW03', sans-serif;}
     .subact_wrap{
         width: 100%;
     }
@@ -222,9 +225,10 @@
         width: 99% !important;
     }
     .el-checkbox__inner{
-        width: 20px !important;
-        height: 20px !important;
-        border: 2px solid var(--border-color-base) !important;
+        width: 21px !important;
+        height: 21px !important;
+        border: 1px solid var(--border-color-base) !important;
+	border-radius: 6px !important;
     }
     .login2-container {
         position: relative;
@@ -233,7 +237,7 @@
         .el-form-item {
             margin-bottom: 0 !important;
             .el-form-item__label {
-                color: rgba(0, 0, 0, 0.6);
+                color: rgba(0, 0, 0, 0.8);
                 line-height: 24px;
                 font-size: 15px;
                 padding-left: 21px;
@@ -262,23 +266,30 @@
                 position: relative !important;
             }
             button {
-                width: 90%;
-                height: 60px;
-                color: rgba(0, 0, 0, 0.6);
+                width: 87%;
+                height: 55px;
+                color: rgba(0, 0, 0, 0.8);
                 background: white;
                 font-size: 16px;
                 font-weight: 700;
-                border-radius: 10px;
+                border-radius: 8px;
             }
             .el-checkbox__label{
-                font-size: 18px;
+                font-size: 17px;
                 color: rgba(0, 0, 0, 0.6);
                 font-weight: 700;
             }
+            .el-button--info.is-plain:focus, .el-button--info.is-plain:hover {
+                background: #3D3F41;
+                border-color: #3D3F41;
+                color: #FFF;
+            }
+            .pwd_reset button {color: rgba(0, 0, 0, 0.6) !important;}
         }
     }
 	.el-checkbox__inner::after{
 		top: 3px !important;
-		left: 6px !important;
+		left: 7px !important;
+		height: 10px;
 	}
 </style>
