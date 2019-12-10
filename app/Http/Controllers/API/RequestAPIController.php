76af88a7b3d9a7b11b1134f347d9a8d6287ba1a4
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-12-10 18:05:28 +0400

    fully delete assignee cols in request_assignees table

--- app/Http/Controllers/API/RequestAPIController.php
+++ app/Http/Controllers/API/RequestAPIController.php
@@ -1413,7 +1413,7 @@ class RequestAPIController extends AppBaseController
         $assigneeData = collect();
         foreach ($data as $single) {
             $type = $single['type'] ?? RequestAssignee::TypeCompetent; // @TODO delete
-            $newAssignee = $this->assignSingleUserToRequest($id, $single['user_id'], $single['role'], $type);
+            $newAssignee = $this->assignSingleUserToRequest($id, $single['user_id'], $type);
             $assigneeData->push($newAssignee);
         }
 
@@ -1438,7 +1438,7 @@ class RequestAPIController extends AppBaseController
      * @param $role
      * @return RequestAssignee|\Illuminate\Database\Eloquent\Model|mixed
      */
-    protected function assignSingleUserToRequest($requestId, $userId, $role, $type)
+    protected function assignSingleUserToRequest($requestId, $userId, $type)
     {
         $user = User::find($userId);
         if (empty($user)) {
@@ -1449,21 +1449,9 @@ class RequestAPIController extends AppBaseController
             return $this->sendError(__('general.invalid_operation'));
         }
 
-        if (in_array($role, PropertyManager::Type)) {
-            $propertyManagerId = PropertyManager::where('user_id', $user->id)->value('id');
-            $assigneeId = $propertyManagerId;
-            $assigneeType = get_morph_type_of(PropertyManager::class);
-        } else {
-            $serviceProviderId = ServiceProvider::where('user_id', $user->id)->value('id');
-            $assigneeId = $serviceProviderId;
-            $assigneeType = get_morph_type_of(ServiceProvider::class);
-        }
-
         return RequestAssignee::updateOrCreate([
             'request_id' => $requestId,
             'user_id' => $userId,
-            'assignee_id' => $assigneeId,
-            'assignee_type' => $assigneeType,
             'type' => $type,
         ], [
             'created_at' => now()
