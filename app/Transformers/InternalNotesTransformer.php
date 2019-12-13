59ff81a2a52402e5a12282dfb6361fd01721c75a
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-12-13 20:26:46 +0400

    InternalNotesTransformer improvement

--- app/Transformers/InternalNotesTransformer.php
+++ app/Transformers/InternalNotesTransformer.php
@@ -5,9 +5,8 @@ namespace App\Transformers;
 use App\Models\InternalNotice;
 
 /**
- * Class BuildingTransformer.
- *
- * @package namespace App\Transformers;
+ * Class InternalNotesTransformer
+ * @package App\Transformers
  */
 class InternalNotesTransformer extends BaseTransformer
 {
@@ -18,20 +17,19 @@ class InternalNotesTransformer extends BaseTransformer
      */
     public function transform(InternalNotice $model)
     {
-        $response = [
-            'id' => $model->id,
-            'request_id' => $model->request_id,
-            'user_id' => $model->user_id,
-            'comment' => $model->comment,
-            'created_at'=> $model->created_at->toDateTimeString(),
-            'updated_at'=> $model->updated_at->toDateTimeString(),
-        ];
-        if ($model->relationExists('user')) {
-            $response['user'] = (new UserTransformer())->transform($model->user);
-        }
-        if ($model->relationExists('users')) {
-            $response['users'] = (new UserTransformer())->transformCollection($model->users);
-        }
+        $response = $this->getAttributesIfExists($model, [
+            'id',
+            'request_id',
+            'user_id',
+            'comment',
+            'created_at',
+            'updated_at'
+        ]);
+
+        $response = $this->includeRelationIfExists($model, $response, [
+            'user' => UserTransformer::class,
+            'users' => UserTransformer::class,
+        ]);
 
         return $response;
     }
