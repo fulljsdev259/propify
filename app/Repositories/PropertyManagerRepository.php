67b7943ba3f6ab634b3b98c839070616a10e89d4
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-12-13 14:53:40 +0400

    filter PM by cpmany name also

--- app/Repositories/PropertyManagerRepository.php
+++ app/Repositories/PropertyManagerRepository.php
@@ -22,6 +22,7 @@ class PropertyManagerRepository extends BaseRepository
         'description' => 'like',
         'first_name' => 'like',
         'last_name' => 'like',
+        'company_name' => 'like',
         'property_manager_format' => 'like',
         'user.email' => 'like',
     ];
