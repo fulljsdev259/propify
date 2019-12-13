2cab2d92caa294a8add7bb91096c6f4f4374c2e4
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-12-13 20:35:43 +0400

    MediaTransformer improvement

--- app/Transformers/MediaTransformer.php
+++ app/Transformers/MediaTransformer.php
@@ -25,18 +25,24 @@ class MediaTransformer extends BaseTransformer
         if (Str::startsWith($url, '//')) {
             $url = Str::replaceFirst('//', '/', $url);
         }
-        $response =  [
-            'id' => $model->id,
-            'name' => $model->file_name,
-            'url' => $url,
-            'collection_name' => $model->collection_name,
-            'model_id' => $model->model_id,
-            'order_column' => $model->order_column,
-            'created_by' => $model->created_at ? $model->created_at->format('d.m.Y') : ''
-        ];
-        if (key_exists('relations_count', $model->getAttributes())) {
-            $response['relations_count'] = $model->relations_count;
-        }
+
+
+        $response = $this->getAttributesIfExists(
+            $model,
+            [
+                'id',
+                'collection_name',
+                'model_id',
+                'order_column',
+                'relations_count',
+            ],
+            [
+                'name' => 'file_name',
+            ]
+        );
+        $response['url'] = $url;
+        $response['created_by'] = $model->created_at ? $model->created_at->format('d.m.Y') : '';
+
         return $response;
     }
 }
