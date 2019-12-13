4ae27cbc4c9948b3a93064e419eeb7b30e9faa1d
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-12-13 20:54:05 +0400

    PinboardTransformer improvement

--- app/Transformers/PinboardTransformer.php
+++ app/Transformers/PinboardTransformer.php
@@ -10,40 +10,49 @@ use App\Models\Pinboard;
  */
 class PinboardTransformer extends BaseTransformer
 {
-    protected $defaultIncludes = [];
-
     /**
      * @param Pinboard $model
      * @return array
      */
     public function transform(Pinboard $model)
     {
-        $ut = new UserTransformer();
-        $tt = new ResidentTransformer();
+        $response = $this->getAttributesIfExists($model, [
+            'id',
+            'type',
+            'sub_type',
+            'is_execution_time',
+            'status',
+            'visibility',
+            'category_image',
+            'category',
+            'content',
+            'title',
+            'created_at',
+            'updated_at',
+            'published_at',
+            'user_id',
+//            'user' => $ut->transform($model->user),
+//            'resident' => $model->user->resident ? $tt->transform($model->user->resident) : null,
+            'comments_count',
+            'announcement',
+            'notify_email',
+            'views_count'
+        ], [
+            'liked',
+            'likes_count' => 'likesCount',
+            'comments_count' => 'all_comments_count'
+        ]);
+        $response = $this->includeRelationIfExists($model, $response, [
+            'user' => UserTransformer::class,
+            'buildings' => BuildingTransformer::class,
+            'quarters' => QuarterTransformer::class,
+            'providers' => ServiceProviderTransformer::class,
+            'likes' => LikeTransformer::class,
+            'media' => MediaTransformer::class,
+            'announcement_email_receptionists' => AnnouncementEmailReceptionistTransformer::class,
 
-        $response = [
-            'id' => $model->id,
-            'type' => $model->type,
-            'sub_type' => $model->sub_type,
-            'is_execution_time' => (bool)$model->is_execution_time,
-            'status' => $model->status,
-            'visibility' => $model->visibility,
-            'category_image' => (bool) $model->category_image,
-            'category' => $model->category,
-            'content' => $model->content,
-            'title' => $model->title,
-            'created_at' => $model->created_at->toDateTimeString(),
-            'updated_at' => $model->updated_at->toDateTimeString(),
-            'published_at' => $model->published_at ? $model->published_at->toDateTimeString() : null,
-            'user_id' => $model->user_id,
-            'user' => $ut->transform($model->user),
-            'resident' => $model->user->resident ? $tt->transform($model->user->resident) : null,
-            'liked' => $model->liked,
-            'likes_count' => $model->likesCount,
-            'comments_count' => $model->all_comments_count,
-            'announcement' => $model->announcement,
-            'notify_email' => $model->notify_email,
-        ];
+        ]);
+        $response['resident'] = $model->user->resident ? (new ResidentTransformer)->transform($model->user->resident) : null;
 
         if ($model->announcement) {
             $response['execution_start'] = $this->formatExecutionTime($model, 'execution_start');
@@ -52,31 +61,8 @@ class PinboardTransformer extends BaseTransformer
             $response['execution_period'] = $model->execution_period;
         }
 
-        if (key_exists('views_count', $model->getAttributes())) {
-            $response['views_count'] = $model->views_count;
-        }
-
-        if ($model->relationExists('buildings')) {
-            $response['buildings'] = (new BuildingTransformer)->transformCollection($model->buildings);
-        }
-        if ($model->relationExists('quarters')) {
-            $response['quarters'] = (new QuarterTransformer)->transformCollection($model->quarters);
-        }
-        if ($model->relationExists('providers')) {
-            $response['providers'] = (new ServiceProviderTransformer)->transformCollection($model->providers);
-        }
-        if ($model->relationExists('likes')) {
-            $response['likes'] = (new LikeTransformer)->transformCollection($model->likes);
-        }
-        if ($model->relationExists('media')) {
-            $response['media'] = (new MediaTransformer)->transformCollection($model->media);
-        }
         if ($model->relationExists('views')) {
             $response['views'] = $model->views->sum('views');
-        }
-        if ($model->relationExists('announcement_email_receptionists')) {
-            $response['announcement_email_receptionists'] = (new AnnouncementEmailReceptionistTransformer())
-                ->transformCollection($model->announcement_email_receptionists);
         }
 
         return $this->addAuditIdInResponseIfNeed($model, $response);
